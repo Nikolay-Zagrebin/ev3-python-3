dfde291564bc3eeb572e28edfcfeadd8c975441f
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2020-06-16 16:05:04 +0300

    Create parameters.py

---
+++ laurensvalk/Ballbot/parameters.py
@@ -0,0 +1,5 @@
+gainGyroAngle                  = 1156*1.4
+gainGyroRate                   = 146*0.85
+gainMotorAngle                 = 7*1
+gainMotorAngularSpeed          = 9*0.95
+gainMotorAngleErrorAccumulated = 0.6
