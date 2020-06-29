81fc0aa900c05b70e8484e3b08cff9c473fee438
Nikolay-Zagrebin <66779368+Nikolay-Zagrebin@users.noreply.github.com>
2020-06-29 13:58:09 +0300

    Create NXTMMX_1.py

---
+++ NXTMMX_1.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.tools import wait
+
+#   ev3-ports:in1:i2c3:M2
+#            "in1:i2c3:M2"
+TACHO_MOTOR = (
+    "/sys/class/tacho-motor/motor5/"
+)
+
+
+def write_tacho(attr, value):
+    with open(TACHO_MOTOR + attr, "w") as f:
+        f.write(value + "\n")
+
+
+def print_tacho(attr):
+    with open(TACHO_MOTOR + attr, "r") as f:
+        print(f.read().strip())
+
+
+# Resetting the Motor
+
+write_tacho("command", "reset")
+
+# Position and Speed
+
+print_tacho("position") # looking at the position attribute
+write_tacho("position", "0") # reset position
+
+# Now, turn the motor one rotation by hand and read the position again…
+
+#print_tacho("position")
+#print_tacho("count_per_rot")
+
+# Running the Motor
+
+# print_tacho("commands") # To find out what commands are available
+
+print("run-forever")
+# write_tacho("speed_sp", "500")
+# write_tacho("command", "run-forever")
+# wait(1000)
+
+# write_tacho("speed_sp", "-1000")
+# write_tacho("command", "run-forever")
+# wait(2000)
+# write_tacho("command", "stop")
+
+print("run-to-abs-pos")
+# write_tacho("command", "run-to-abs-pos")
+# wait(10000)
+
+print("run-to-rel-pos")
+# write_tacho("position_sp", "180")
+# write_tacho("command", "run-to-rel-pos")
+# wait(10000)
+
+print("run-timed")
+# write_tacho("time_sp", "2000")
+# write_tacho("command", "run-timed")
+
+# Не поддерживается
+print("run-direct")
+# write_tacho("duty_cycle_sp", "20")
+# write_tacho("command", "run-direct")
+# wait(10000)
+
+# Stopping the Motor
+print_tacho("stop_actions") # To find out what stop_actions are available
+
+print("coast")
+# write_tacho("speed_sp", "1000")
+# write_tacho("time_sp", "1000")
+# write_tacho("stop_action", "coast")
+# write_tacho("command", "run-timed")
+# wait(10000)
+
+print("brake")
+# write_tacho("speed_sp", "1000")
+# write_tacho("time_sp", "1000")
+# write_tacho("stop_action", "brake")
+# write_tacho("command", "run-timed")
+# wait(10000)
+
+print("hold")
+# write_tacho("speed_sp", "1000")
+# write_tacho("position_sp", "180")
+# write_tacho("stop_action", "hold")
+# write_tacho("command", "run-to-rel-pos")
+# wait(10000)
+
+# Polarity
+print_tacho("polarity")
+
+# write_tacho("polarity", "inversed")
+write_tacho("speed_sp", "300")
+write_tacho("command", "run-forever")
+for _ in range(100):
+    print_tacho("position")
+    wait(10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/usr/bin/env python3
+# from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank
+# from ev3dev2.sensor import INPUT_1
+# from ev3dev2.sensor.lego import TouchSensor
+# from ev3dev2.led import Leds
+
+# m = LargeMotor("in1:i2c3:M2")
+# m.on_for_rotations(SpeedPercent(100), 10)
